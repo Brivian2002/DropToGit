a2aa5c087864a0f0b047f499fdf324924c1b0ea0
Bright Dumashie <161515806+Brivian2002@users.noreply.github.com>
2026-08-15 15:25:47 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
   typescript: {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
-  allowedDevOrigins: ["*.space-z.ai"],
 };
 
 export default nextConfig;
